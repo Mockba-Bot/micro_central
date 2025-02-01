2c3f412f934a05787f274851936af18062a2d671
dvconsultores <adominguez@dvconsultores.com>
2025-01-31 21:40:35 -0400

    Specify 'central' queue for send_telegram_message_task in celery_tasks.py

--- app/tasks/celery_tasks.py
+++ app/tasks/celery_tasks.py
@@ -4,7 +4,7 @@ from app.utils.send_bot_notification import send_telegram_message
 
 logger = logging.getLogger(__name__)
 
-@celery_app.task
+@celery_app.task(queue="central")
 def send_telegram_message_task(token, message):
     logger.info(f"Sending telegram message to {token}")
     send_telegram_message(token, message)
