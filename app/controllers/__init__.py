2a1b7ae174b339fd92773d21e3daf918586be438
dvconsultores <adominguez@dvconsultores.com>
2025-02-28 20:09:12 -0400

    Remove BinanceController and its associated routes and logic

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,4 +1,3 @@
-from .BinanceController import router as binance_router
 from .TBotStatusController import router as t_bot_status_router
 from .TLoginController import router as tlogin_router
 from .TSignalController import router as tsignal_router
